e865a8ff75969f2835e539631f180a17b68e8c01
Zeeshan Haider Soomro <zh7774440@gmail.com>
2024-04-12 22:18:09 +0500

    simple cli calculator

--- index.ts
+++ index.ts
@@ -9,12 +9,12 @@ const answer = await inquirer.prompt([
     message: "Select one of the operators to perform operation",
     type: "list",
     name: "operator",
-    choices: ["Addition", "Subtraction", "multiplication", "division"],
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
   },
 ]);
 
 
-if (answer.operator === "Additione") {
+if (answer.operator === "Addition") {
     console.log(answer.firstNumber + answer.secondNumber);
 } else if (answer.operator === "Subtraction") {
     console.log(answer.firstNumber - answer.secondNumber);
